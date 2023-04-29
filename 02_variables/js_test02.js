a4319710f938079deb051edd989f803a8d65fb7c
lgswin <lgswin@gmail.com>
2023-04-29 21:19:10 +0900

    js : variables

---
+++ 02_variables/js_test02.js
@@ -0,0 +1,24 @@
+// 재사용 가능
+// 변수 선언
+let a = 2;
+let b = 5;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+// 값(데이터)의 재할당 가능
+let c = 12;
+console.log(c);
+
+c = 999;
+console.log(c)
+
+// 값(데이터)의 재할당 불가능
+const d = 12;
+console.log(d);
+// d = 999;
+// Uncaught TypeError TypeError: Assignment to constant variable.
+// var 는 사용하지 않는 것을 권장
+
